4991c4864afa6d0ea07fc8f42e11a2fc00903860
Matt Seymour <matt@actual-experience.com>
2013-03-17 12:38:07 +0000

    Data from user

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -54,7 +54,7 @@ def data(request):
     _list = Report.objects.all()[:100]
     _l = []
     for i in _list:
-         j = {'report_type':i.report_type, 'y':i.report_northing, 'x':i.report_easting}
+         j = {'description':i.description,'report_type':i.report_type, 'y':i.report_northing, 'x':i.report_easting}
          _l.append(j)
     return HttpResponse(json.dumps(_l), mimetype='application/json')
 
